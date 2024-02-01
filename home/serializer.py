98310891753440364d2d9d12981ec8077ca18ed7
AYUB THAPA <68695090+AYUB-CMD@users.noreply.github.com>
2024-02-02 00:42:32 +0530

    part 1

--- home/serializer.py
+++ home/serializer.py
@@ -1,12 +1,33 @@
 from rest_framework import serializers
 from .models import *
+import re
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'
-        # fields = ['__all__']
-        # exclude=['']
+        # fields = ['title']
+        # exclude=['description']
         
-        # def validate(self,validated_data):
-        #     if validated_data.get('title'):
+ # if you want to validate only single field 
+    def validate_title(self, data):
+        title = data
+        
+        if title:
+            regex = re.compile(r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[\w\d]{8,}$')
+            
+            if not regex.search(title):
+                raise serializers.ValidationError('Title cannot contain special characters')
+        
+        return data
+        
+    # def validate(self, validated_data):
+    #     title = validated_data.get('title')
+        
+    #     if title:
+    #         regex = re.compile(r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[\w\d]{8,}$')
+            
+    #         if not regex.search(title):
+    #             raise serializers.ValidationError('Title cannot contain special characters')
+        
+    #     return validated_data
