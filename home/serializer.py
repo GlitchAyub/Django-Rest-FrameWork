d97512d8ce32336c3f93fa8c2ae2ae281bcc3917
AYUB THAPA <68695090+AYUB-CMD@users.noreply.github.com>
2024-02-03 21:31:18 +0530

    part-2 of serializer

--- home/serializer.py
+++ home/serializer.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import *
 import re
+from django.template.defaultfilters import slugify
+
 
 class TodoSerializer(serializers.ModelSerializer):
+    slug = serializers.SerializerMethodField()
     class Meta:
         model = Todo
         fields = '__all__'
@@ -10,19 +13,10 @@ class TodoSerializer(serializers.ModelSerializer):
         # exclude=['description']
         
  # if you want to validate only single field 
-    def validate_title(self, data):
-        title = data
-        
-        if title:
-            regex = re.compile(r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[\w\d]{8,}$')
-            
-            if not regex.search(title):
-                raise serializers.ValidationError('Title cannot contain special characters')
-        
-        return data
-        
-    # def validate(self, validated_data):
-    #     title = validated_data.get('title')
+    def get_slug(self,obj):
+        return slugify(obj.title)
+    # def validate_title(self, data):
+    #     title = data
         
     #     if title:
     #         regex = re.compile(r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[\w\d]{8,}$')
@@ -30,4 +24,17 @@ class TodoSerializer(serializers.ModelSerializer):
     #         if not regex.search(title):
     #             raise serializers.ValidationError('Title cannot contain special characters')
         
-    #     return validated_data
+    #     return data
+        
+    def validate(self, validated_data):
+        title = validated_data.get('title')
+        
+        if title:
+            regex = re.compile(r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[\w\d]{8,}$')
+            
+            if not regex.search(title):
+                raise serializers.ValidationError('Title cannot contain special characters')
+        
+        return validated_data
+    
+ 
--- home/views.py
+++ home/views.py
@@ -39,30 +39,32 @@ def get_todo(request):
                 "message": "success",
                 'data': serializer.data
             })
-    
+
+
 @api_view(['POST'])
 def post_todo(request):
     try:
         data = request.data
         serializer = TodoSerializer(data=data)
+
         if serializer.is_valid():
             serializer.save()
             print(serializer.data)
-            
             return Response({
                 "status": True,
-                "message": "success",
+                "message": "Success",
                 'data': serializer.data
             })
 
         return Response({
             "status": False,
-            "message": "invalid-data",
+            "message": "Invalid data",
             'data': serializer.errors
         })
+
     except Exception as e:
         print(e)
         return Response({
             "status": False,
-            "message": "invalid-data",
+            "message": "Invalid data",
         })
